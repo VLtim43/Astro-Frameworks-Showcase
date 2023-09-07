ba07135865b8d0c2fe21f935c287db6f137ce449
VLtim43 <fernandoreal.valentim@gmail.com>
2023-09-07 12:24:24 -0300

    Lit element

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,13 @@ import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
 import solidJs from "@astrojs/solid-js";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [analogjsangular(), react(), vue(), svelte(), solidJs(), preact({
     include: ["**/preact/*"]
-  }), tailwind()]
+  }), tailwind(), lit()]
 });
---
+++ src/components/LitComponent.js
@@ -0,0 +1,34 @@
+import { LitElement, html, css } from "lit";
+
+export class LitComponent extends LitElement {
+  static styles = css`
+    .container {
+      min-width: 264px;
+      max-width: 24rem;
+      margin: 0.25rem 0;
+      border-radius: 0.25rem;
+      overflow: hidden;
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
+        0 1px 2px 0 rgba(0, 0, 0, 0.06);
+      background-color: #67e8f9;
+      padding: 1rem;
+      border-left: 0.25rem solid #4a5568;
+    }
+
+    .text {
+      margin: 0;
+      color: white;
+      font-size: 1rem;
+    }
+  `;
+
+  render() {
+    return html`
+      <div class="container">
+        <p class="text">Hello from Lit!!</p>
+      </div>
+    `;
+  }
+}
+
+customElements.define("lit-component", LitComponent);
