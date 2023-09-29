5803d1a46a46ba5c74c53faa23d2ed272af23e44
VLtim43 <fernandoreal.valentim@gmail.com>
2023-09-29 18:15:17 -0300

    React component

--- src/components/ReactComponent.tsx
+++ src/components/ReactComponent.tsx
@@ -1,7 +1,32 @@
 export const ReactComponent: React.FC = () => {
   return (
-    <div className="min-w-[300px] max-w-sm my-1 rounded overflow-hidden shadow-lg bg-blue-500 p-4 border-l-4 border-gray-700">
-      <p className="text-gray-300 text-base">Hello from React!!</p>
+    <div className="relative flex flex-col justify-between h-full w-full bg-gradient-to-br from-sky-600 to-blue-600 text-gray-200">
+      <div className="flex justify-end px-2 pt-1">
+        <h1 className="font-onest text-xs text-gray-300">Made with React</h1>
+      </div>
+
+      <div className="font-onest px-4 ">
+        <h1 className="text-4xl text-left font-onest font-bold">React</h1>
+        <h3 className="py-3">
+          The most famous library for web and native user interfaces
+        </h3>
+        <a
+          href="https://react.dev/"
+          className="text-white hover:text-blue-700 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View the documentation
+        </a>
+      </div>
+
+      <div className="flex justify-end ">
+        <img
+          src="/reactLogo.svg"
+          alt="React Logo"
+          className="w-7 h-7 m-1 p-0.5"
+        />
+      </div>
     </div>
   );
 };
