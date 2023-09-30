e9c9db23dce8a266eefcc1bff7971261d5edeacd
VLtim43 <fernandoreal.valentim@gmail.com>
2023-09-30 17:25:30 -0300

    add svelt

--- src/components/SolidComponent.tsx
+++
@@ -1,9 +0,0 @@
-const SolidComponent = () => {
-  return (
-    <div className="min-w-[300px] max-w-sm my-1 rounded overflow-hidden shadow-lg bg-blue-800 p-4 border-l-4 border-gray-700">
-      <p className="text-gray-300 text-base">Hello from Solid!!</p>
-    </div>
-  );
-};
-
-export default SolidComponent;
